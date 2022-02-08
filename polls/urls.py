a0273452420753161ac5762db22fe64dbe72142e
mink-yung <81360758+mink-yung@users.noreply.github.com>
2022-02-08 16:39:37 +0900

    초기파일

---
+++ polls/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    re_path(r'^(?P<question_id>[0-9]+)/$', views.detail),
+    re_path(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    re_path(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
+]
